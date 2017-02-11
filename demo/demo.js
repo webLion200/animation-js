98ab817787f008834ebf5a56cea3fbbd4ff9bb5f
web_lion <794358525@qq.com>
2017-02-11 10:52:22 +0800

    demo 完成

---
+++ demo/demo.js
@@ -0,0 +1,21 @@
+var imgUrl = 'rabbit-big.png';
+var ele = document.getElementById('rabit');
+var positions = ['0 -854', '-172 -852', '-349 -852', '-524 -852', '-698 -850', '-874 -847']
+
+animation(ele, positions, imgUrl);
+function animation(ele, positions, imgUrl) {
+	ele.style.backgroundImage = 'url(' + imgUrl + ')';
+	ele.style.backgroundRepeat = 'no-repeat';
+	var index = 0;
+
+	function run() {
+		var position = positions[index].split(' ');
+		ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px';
+		index++;
+		if (index >= positions.length) {
+			index = 0;
+		}
+		setTimeout(run, 80);
+	}
+	run();
+}
